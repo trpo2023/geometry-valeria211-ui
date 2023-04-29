38d28af90449831e7b80fd64f9822a3c4914c37f
valeria211-ui <valeria.panchina@yandex.ru>
2023-04-29 18:09:48 +0700

    Change

---
+++ build/CMakeFiles/test_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_app.dir/test/main.cpp.o"
+  "CMakeFiles/test_app.dir/test/main.cpp.o.d"
+  "CMakeFiles/test_app.dir/test/test.cpp.o"
+  "CMakeFiles/test_app.dir/test/test.cpp.o.d"
+  "test_app"
+  "test_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
